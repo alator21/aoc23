0d98ce34e5d05d6fb4351dbaa532d2fe8ba3cf2f
alator21 <botazz133@gmail.com>
2023-12-02 14:20:05 +0200

    Add template

---
+++ src/TEMPLATE/Template.ts
@@ -0,0 +1,29 @@
+import {prettyPrintSolution, readInputContents, Solution} from "../utils.ts";
+
+class DayXPart1 implements Solution {
+    day(): number {
+        return 2;
+    }
+
+    part(): 1 | 2 {
+        return 1;
+    }
+
+    input(): number {
+        return 1;
+    }
+
+    async result(): Promise<string> {
+        const rawInput = await readInputContents(this.day(), this.input());
+        for (const line of rawInput.split('\n')) {
+        }
+        return '42';
+    }
+
+    expectedResult(): string {
+        return "2369";
+    }
+}
+
+const solution = new DayXPart1();
+await prettyPrintSolution(solution);
