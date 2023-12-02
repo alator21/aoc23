f62ac63a9991858d35c3adb12cf46a40bc460ca3
alator21 <botazz133@gmail.com>
2023-12-02 14:13:15 +0200

    Rewrite D2P2

--- src/2/part2.ts
+++ src/2/part2.ts
@@ -1,57 +1,85 @@
-import {readInputContents} from "../utils.ts";
+import {prettyPrintSolution, readInputContents, Solution} from "../utils.ts";
 
-const rawInput = await readInputContents(2, 2);
-
-
-const quantitiesPerColorPerGame: Map<string/*gameId*/, Map<string/*color*/, number/*quantity*/>> = new Map();
-for (const line of rawInput.split('\n')) {
-    // console.log(line);
-    const gamePart = line.split(':').at(0);
-    if (gamePart === undefined) {
-        throw new Error('Could not find game part');
+class Day2Part2 implements Solution {
+    day(): number {
+        return 2;
     }
-    const gameId = gamePart.split(' ').at(1);
-    if (gameId === undefined) {
-        throw new Error(`Invalid gameId: ${gameId}`)
-    }
-    // console.log(gameId)
-    const game = line.substring(gamePart.length + 1, line.length);
 
-    const rounds = game.split(';').map(r => r.trim())
-    // console.log(rounds);
-    const quantitiesPerColorOfGame: Array<Map<string, number>> = [];
-    for (const round of rounds) {
-        const reveals = round.split(',').map(r => r.trim());
-        // console.log(reveals)
+    input(): number {
+        return 2;
+    }
+
+    async result(): Promise<string> {
+        const rawInput = await readInputContents(this.day(), this.input());
+        const quantitiesPerColorPerGame: Array<Map<string/*color*/, number/*quantity*/>> = [];
+        for (const line of rawInput.split('\n')) {
+            // console.log(line);
+            const gamePart = line.split(':').at(0);
+            if (gamePart === undefined) {
+                throw new Error(`Could not find game part of line: ${line}`);
+            }
+            const roundsPart = line.substring(gamePart.length + 1, line.length);
+
+            const rounds = roundsPart.split(';').map(r => r.trim())
+            const quantitiesPerColorOfGame = this.processRounds(rounds);
+            const quantityPerColorNormalized: Map<string, number> = this.normalizeResult(quantitiesPerColorOfGame);
+
+            quantitiesPerColorPerGame.push(quantityPerColorNormalized);
+        }
+        let sum = 0;
+        for (const quantityPerColorOfGame of quantitiesPerColorPerGame) {
+            const partialSum = Array.from(quantityPerColorOfGame.values()).reduce((partial, n) => partial * n, 1);
+            sum += partialSum;
+        }
+        return sum.toString(10);
+    }
+
+    processRounds(rounds: string[]): Array<Map<string, number>> {
+        const quantitiesPerColorOfGame: Array<Map<string, number>> = [];
+        for (const round of rounds) {
+            const reveals = round.split(',').map(r => r.trim());
+            const quantityPerColor = this.processReveals(reveals);
+            quantitiesPerColorOfGame.push(quantityPerColor)
+        }
+        return quantitiesPerColorOfGame;
+    }
+
+    processReveals(reveals: string[]): Map<string, number> {
         const quantityPerColor: Map<string, number> = new Map();
-        quantitiesPerColorOfGame.push(quantityPerColor)
         for (const reveal of reveals) {
-            const [quantityAsString, color] = reveal.split(' ');
-            // console.log(quantityAsString, color)
-            const quantity = Number(quantityAsString);
-            quantityPerColor.set(color, quantity)
+            const [color, quantity] = this.processReveal(reveal);
+            quantityPerColor.set(color, quantity);
         }
+        return quantityPerColor;
+    }
 
+    processReveal(reveal: string): [string, number] {
+        const [quantityAsString, color] = reveal.split(' ');
+        const quantity = Number(quantityAsString);
+        return [color, quantity];
     }
-    const quantityPerColorNormalized: Map<string, number> = new Map();
-    for (const quantityPerColor of quantitiesPerColorOfGame) {
-        for (const [color, quantity] of quantityPerColor.entries()) {
-            const quantityOfColor = quantityPerColorNormalized.get(color);
-            if (quantityOfColor === undefined) {
-                quantityPerColorNormalized.set(color, quantity);
-                continue;
-            }
-            if (quantity > quantityOfColor) {
-                quantityPerColorNormalized.set(color, quantity);
+
+    normalizeResult(quantitiesPerColorOfGame: Array<Map<string, number>>): Map<string, number> {
+        const quantityPerColorNormalized: Map<string, number> = new Map();
+        for (const quantityPerColor of quantitiesPerColorOfGame) {
+            for (const [color, quantity] of quantityPerColor.entries()) {
+                const quantityOfColor = quantityPerColorNormalized.get(color);
+                if (quantityOfColor === undefined) {
+                    quantityPerColorNormalized.set(color, quantity);
+                    continue;
+                }
+                if (quantity > quantityOfColor) {
+                    quantityPerColorNormalized.set(color, quantity);
+                }
             }
         }
+        return quantityPerColorNormalized;
     }
-    quantitiesPerColorPerGame.set(gameId, quantityPerColorNormalized);
-    // console.log('--')
+
+    expectedResult(): string {
+        return "66363";
+    }
 }
-let sum = 0;
-for (const quantityPerColorOfGame of quantitiesPerColorPerGame.values()) {
-    const partialSum = Array.from(quantityPerColorOfGame.values()).reduce((partial, n) => partial * n, 1);
-    sum += partialSum;
-}
-console.log(sum);
+
+const solution = new Day2Part2();
+await prettyPrintSolution(solution);
