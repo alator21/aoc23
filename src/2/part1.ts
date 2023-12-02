a10317565e5782755ae4cd390a5bbbe160bdc075
alator21 <botazz133@gmail.com>
2023-12-02 13:53:53 +0200

    Rewrite D2P1

--- src/2/part1.ts
+++ src/2/part1.ts
@@ -1,64 +1,92 @@
-import {readInputContents} from "../utils.ts";
+import {prettyPrintSolution, readInputContents, Solution} from "../utils.ts";
 
-const rawInput = await readInputContents(2, 2);
+class Day2Part1 implements Solution {
+    private static readonly TARGETS: Map<string, number> = new Map([
+        ['red', 12],
+        ['green', 13],
+        ['blue', 14],
+    ]);
 
+    private readonly allGames: string[] = [];
+    private readonly invalidGames: string[] = [];
 
-const TARGETS: Map<string, number> = new Map([
-    ['red', 12],
-    ['green', 13],
-    ['blue', 14],
-]);
-
-
-const allGames: string[] = [];
-const invalidGames: string[] = [];
-for (const line of rawInput.split('\n')) {
-    // console.log(line);
-    const gamePart = line.split(':').at(0);
-    if (gamePart === undefined) {
-        throw new Error('Could not find game part');
+    day(): number {
+        return 2;
     }
-    const gameId = gamePart.split(' ').at(1);
-    if (gameId === undefined){
-        throw new Error(`Invalid gameId: ${gameId}`)
-    }
-    allGames.push(gameId);
-    // console.log(gameId)
-    const game = line.substring(gamePart.length + 1, line.length);
 
-    const rounds = game.split(';').map(r => r.trim())
-    // console.log(rounds);
-    let revealInvalid = false;
-    for (const round of rounds) {
-        if (revealInvalid) {
-            break;
+    input(): number {
+        return 2;
+    }
+
+
+    async result(): Promise<string> {
+        const rawInput = await readInputContents(this.day(), this.input());
+        for (const line of rawInput.split('\n')) {
+            // console.log(line);
+            const gamePart = line.split(':').at(0);
+            if (gamePart === undefined) {
+                throw new Error(`Could not find game part of line: ${line}`);
+            }
+            const gameId = this.extractGameId(gamePart);
+            this.allGames.push(gameId);
+            // console.log(gameId)
+            const roundsPart = line.substring(gamePart.length + 1, line.length);
+            const rounds = roundsPart.split(';').map(r => r.trim())
+            try {
+                this.processRounds(rounds)
+            } catch (e) {
+                this.invalidGames.push(gameId)
+            }
+
         }
-        const reveals = round.split(',').map(r => r.trim());
-        // console.log(reveals)
+        const validGames = this.allGames.filter(g => !this.invalidGames.includes(g))
+        const sumOfValidGames = validGames.reduce((partial, n) => partial + Number(n), 0);
+        return sumOfValidGames.toString(10);
+    }
+
+    processRounds(rounds: string[]): void {
+        for (const round of rounds) {
+            const reveals = round.split(',').map(r => r.trim());
+            this.processReveals(reveals)
+        }
+    }
+
+    processReveals(reveals: string[]): void {
         for (const reveal of reveals) {
-            const [quantityAsString, color] = reveal.split(' ');
-            // console.log(quantityAsString, color)
-
-            const targetColorMax = TARGETS.get(color);
-            if (targetColorMax === undefined) {
-                throw new Error(`wrong color: ${color}`)
-            }
-
-            const quantity = Number(quantityAsString);
-            if (quantity > targetColorMax) {
-                invalidGames.push(gameId);
-                revealInvalid = true;
-                break;
-            }
-
+            this.processReveal(reveal)
         }
     }
-    // console.log('--')
 
+    processReveal(reveal: string): void {
+        if (this.isRevealInvalid(reveal)) {
+            throw new Error('invalid reveal')
+        }
+
+    }
+
+    isRevealInvalid(reveal: string): boolean {
+        const [quantityAsString, color] = reveal.split(' ');
+        // console.log(quantityAsString, color)
+        const targetColorMax = Day2Part1.TARGETS.get(color);
+        if (targetColorMax === undefined) {
+            throw new Error(`wrong color: ${color}`)
+        }
+        const quantity = Number(quantityAsString);
+        return quantity > targetColorMax;
+    }
+
+    extractGameId(gamePart: string): string {
+        const gameId = gamePart.split(' ').at(1);
+        if (gameId === undefined) {
+            throw new Error(`Invalid gameId: ${gameId}`)
+        }
+        return gameId;
+    }
+
+    expectedResult(): string {
+        return "2369";
+    }
 }
-const validGames = allGames.filter(g => !invalidGames.includes(g))
-// console.log(allGames);
-// console.log(invalidGames)
-// console.log(validGames)
-const sumOfValidGames = validGames.reduce((partial, n) => partial + Number(n), 0);
-console.log(sumOfValidGames)
+
+const solution = new Day2Part1();
+await prettyPrintSolution(solution);
