e840c68a6c9d527821cb68c93cfffed6b5414823
alator21 <botazz133@gmail.com>
2023-12-07 20:42:11 +0200

    D6P2

--- src/6/part2.ts
+++ src/6/part2.ts
@@ -2,13 +2,13 @@ import {prettyPrintSolution, readInputContents, Solution} from "../utils/utils.t
 import {splitBySpaces} from "../utils/StringOperations.ts";
 import {productOfCollection} from "../utils/CollectionOperations.ts";
 
-class Day6Part1 implements Solution {
+class Day6Part2 implements Solution {
     day(): number {
         return 6;
     }
 
     part(): 1 | 2 {
-        return 1;
+        return 2;
     }
 
     input(): number {
@@ -21,14 +21,16 @@ class Day6Part1 implements Solution {
         const rawInput = await readInputContents(this.day(), this.input());
         for (const line of rawInput.split('\n')) {
             if (line.includes('Time')) {
-                times.push(...splitBySpaces(line.split(':')[1].trim()).map(r => Number(r)));
+                const t = splitBySpaces(line.split(':')[1].trim()).map(r => Number(r));
+                times.push(Number(t.reduce((partial, current) => partial+current, '')));
             }
             if (line.includes('Distance')) {
-                distances.push(...splitBySpaces(line.split(':')[1].trim()).map(r => Number(r)));
+                const t = splitBySpaces(line.split(':')[1].trim()).map(r => Number(r));
+                distances.push(Number(t.reduce((partial, current) => partial+current, '')));
             }
         }
 
-        const combinations:number[]=[];
+        const combinations: number[] = [];
         for (let i = 0; i < times.length; i++) {
             const time = times[i];
             const distance = distances[i];
@@ -36,26 +38,23 @@ class Day6Part1 implements Solution {
             combinations.push(numberOfCombinationsToBeatIt);
         }
         return productOfCollection(combinations).toString(10);
-        // console.log(times);
-        // console.log(distances);
-        // return '42';
     }
 
     expectedResult(): string {
-        return "128700";
+        return "39594072";
     }
 
     private calculateNumberOfCombinationsToBeatIt(time: number, distance: number): number {
         let counter = 0;
         for (let i = 1; i < time - 1; i++) {
             const currentSpeed = i * (time - i);
-            if (currentSpeed > distance){
-               counter++;
+            if (currentSpeed > distance) {
+                counter++;
             }
         }
         return counter;
     }
 }
 
-const solution = new Day6Part1();
+const solution = new Day6Part2();
 await prettyPrintSolution(solution);
